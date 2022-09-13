a2f9cddd3eecfb79bde9f062141eae1eba90c773
Damla Osman <damlaaosman@gmail.com>
2022-09-13 19:55:27 +1000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    pattern = /[.!?]+/
+    self.split(pattern).count
   end
+
+  # def return_self
+  #   puts self
+  # end
+
 end
+
+# String.new("hello world").return_self
